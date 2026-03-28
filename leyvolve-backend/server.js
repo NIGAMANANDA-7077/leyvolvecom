4e644257638554c2f121602ff5262e4f7660ae05
Nigamananda <ngaranaik@gmail.com>
2026-03-28 14:29:15 +0530

    Update nodemailer config for better stability

--- leyvolve-backend/server.js
+++ leyvolve-backend/server.js
@@ -17,13 +17,14 @@ app.get('/', (req, res) => {
 
 // Nodemailer Transporter
 const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, // use SSL
+    service: 'gmail',
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    }
+    },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000
 });
 
 // Verify connection configuration
